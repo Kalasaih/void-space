2a427cf3c951240bcb7178dbe5455b67f09d151a
Gregoire Charlot <charlot.gregoire@gmail.com>
2025-03-17 11:19:10 +0100

    various bug fix - code optimisation - comment refactoring

--- scene/spawner/klaed_spawner.gd
+++ scene/spawner/klaed_spawner.gd
@@ -1,13 +1,13 @@
-#Ennemy Spawner
+# Klaed Spawner
 extends Node2D
 
-# variable
+# Variable
 @onready var spawn_point = $SpawnPoint
+@onready var klaed_fighter_scene = preload("res://scene/ennemy/klaed_fighter.tscn")
 
-# managing spawn by waves with timer
+# Managing spawn by waves with timer
 func _on_spawn_timer_timeout() -> void:
 	var random_spawn_point = spawn_point.get_children().pick_random()
-	var klaed_fighter_scene = preload("res://scene/ennemy/klaed_fighter.tscn")
 	var klaed_fighter = klaed_fighter_scene.instantiate()
 	klaed_fighter.global_position = random_spawn_point.global_position
 	owner.add_child(klaed_fighter)
